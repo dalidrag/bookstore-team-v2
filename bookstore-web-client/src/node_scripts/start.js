12752727a1c6ef3f67449ff10c278f3ea50d14ff
Dalibor Dragojevic <dalibor.dragojevic@computerrock.com>
2020-01-02 07:49:47 +0100

    webpack middleware eslint config

---
+++ bookstore-web-client/src/node_scripts/start.js
@@ -0,0 +1,150 @@
+"use strict";
+
+const path = require("path");
+const fs = require("fs");
+
+// set environment
+process.env.BABEL_ENV = "development";
+process.env.NODE_ENV = "development";
+
+// exit on unhandled rejections
+process.on("unhandledRejection", err => {
+  throw err;
+});
+
+const chalk = require("chalk");
+const webpack = require("webpack");
+const express = require("express");
+
+const WebpackDevMiddleware = require("webpack-dev-middleware");
+const clearConsole = require("@ateam/react-dev-utils/clearConsole");
+const formatWebpackMessages = require("@ateam/react-dev-utils/formatWebpackMessages");
+const { choosePort } = require("@ateam/react-dev-utils/WebpackDevServerUtils");
+
+const config = require("../../webpack.config.js");
+
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
+const isInteractive = process.stdout.isTTY;
+
+if (process.env.HOST) {
+  console.log(
+    chalk.cyan(
+      `Attempting to bind to HOST environment variable: ${chalk.yellow(
+        chalk.bold(process.env.HOST)
+      )}`
+    )
+  );
+  console.log(
+    `If this was unintentional, check that you haven't mistakenly set it in your shell.`
+  );
+  console.log(`Learn more here: ${chalk.yellow("http://bit.ly/2mwWSwH")}`);
+  console.log();
+}
+
+choosePort(HOST, DEFAULT_PORT)
+  .then(port => {
+    if (port == null) {
+      return;
+    }
+
+    // create webpack compiler
+    let compiler;
+    try {
+      compiler = webpack(config);
+    } catch (err) {
+      console.log(chalk.red("Failed to compile."));
+      console.log();
+      console.log(err.message || err);
+      console.log();
+      process.exit(1);
+    }
+
+    // on webpack invalidation display pending status
+    compiler.plugin("invalid", () => {
+      if (isInteractive) {
+        clearConsole();
+      }
+      console.log("Compiling...");
+    });
+
+    // on webpack compile display result status and formatted stats
+    compiler.plugin("done", stats => {
+      if (isInteractive) {
+        clearConsole();
+      }
+
+      const messages = formatWebpackMessages(stats.toJson({}, true));
+      const isSuccessful = !messages.errors.length && !messages.warnings.length;
+      if (isSuccessful) {
+        console.log(chalk.green("Compiled successfully!"));
+        console.log();
+        console.log(chalk.gray(`Listening on http://localhost:${port}`));
+        console.log();
+      }
+
+      // if errors exist, only show errors
+      if (messages.errors.length) {
+        console.log(chalk.red("Failed to compile.\n"));
+        console.log(messages.errors.join("\n\n"));
+        console.log();
+        return;
+      }
+
+      // show warnings if no errors were found
+      if (messages.warnings.length) {
+        console.log(chalk.yellow("Compiled with warnings.\n"));
+        console.log(messages.warnings.join("\n\n"));
+        console.log();
+      }
+    });
+
+    // initialize dev server
+    const devServer = express();
+
+    // configure WebpackDevMiddleware
+    devServer.use(
+      WebpackDevMiddleware(compiler, {
+        compress: true,
+        contentBase: "../../public",
+        watchContentBase: true,
+        publicPath: "/",
+        // display only error messages, formatted warning stats will be shown in terminal
+        logLevel: "error",
+        quiet: true,
+        // do not watch node_modules
+        watchOptions: {
+          ignored: /node_modules/
+        }
+      })
+    );
+
+    // serve files from the public dir
+    const appDirectory = fs.realpathSync(process.cwd());
+    const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+    devServer.use(express.static(resolveApp("public")));
+
+    // launch webpack dev server
+    devServer.listen(port, HOST, err => {
+      if (err) {
+        return console.log(err);
+      }
+      if (isInteractive) {
+        clearConsole();
+      }
+      console.log(chalk.cyan("Starting the development server...\n"));
+    });
+
+    ["SIGINT", "SIGTERM"].forEach(function(sig) {
+      process.on(sig, function() {
+        // devServer.close();
+        process.exit();
+      });
+    });
+  })
+  .catch(err => {
+    if (err && err.message) {
+      console.log(err.message);
+    }
+    process.exit(1);
+  });
--- bookstore-web-client/webpack.config.js
+++ bookstore-web-client/webpack.config.js
@@ -1,36 +1,51 @@
-module.exports = env => {
-    process.env.NODE_ENV = env.NODE_ENV;
+const eslintFormatter = require("@ateam/react-dev-utils/eslintFormatter");
 
-    return {
-        // 1
-        entry: './src/index.tsx',
-        // 4
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: ['babel-loader']
-                },
-                {
-                    test: /\.(ts|tsx)$/,
-                    exclude: /node_modules/,
-                    use: ['babel-loader', 'ts-loader']
-                }
-            ]
-        },
-        resolve: {
-            extensions: ['*', '.js', '.jsx']
-        },
-        // 2
-        output: {
-            path: __dirname + '/public',
-            publicPath: '/',
-            filename: 'bundle.js'
-        },
-        // 3
-        devServer: {
-            contentBase: './public'
-        }
-    }
+module.exports = {
+  // 1
+  entry: "./src/index.tsx",
+  mode: "development",
+  // 4
+  module: {
+    rules: [
+      // eslint
+      {
+        test: /\.(js|jsx|mjs|ts|tsx)$/,
+        enforce: "pre",
+        use: [
+          {
+            loader: require.resolve("eslint-loader"),
+            options: {
+              formatter: eslintFormatter,
+              eslintPath: require.resolve("eslint"),
+              fix: true
+            }
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  // 2
+  output: {
+    path: __dirname + "/public",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  // 3
+  devServer: {
+    contentBase: "./public"
+  }
 };
